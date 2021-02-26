11c5dbdbc07d8a05c45340754fdacd6f609c73dd
Sadanand Rai <sadanand.r@dashtechinc.com>
2021-02-26 15:33:52 +0530

    add color extension and general file

---
+++ lib/GeneralUtils/Utils.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Utils {
+
+  static void showLoader(bool show, BuildContext context) {
+    if (show) {
+      showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return Center(
+                child: SpinKitCircle(
+                  color: Colors.black,
+                  size: 80,
+                ));
+          });
+    } else {
+      Navigator.of(context, rootNavigator: true).pop("");
+    }
+  }
+
+  static Future<dynamic> navigateToScreen(
+      BuildContext context, Widget screen) async {
+    var value = await Navigator.push(
+        context, MaterialPageRoute(builder: (context) => screen));
+    return value;
+  }
+
+  static navigateReplaceToScreen(BuildContext context, Widget screen) {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => screen));
+  }
+
+  static bool isValidEmail(String email) {
+    bool result = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(email);
+    return result;
+  }
+}
